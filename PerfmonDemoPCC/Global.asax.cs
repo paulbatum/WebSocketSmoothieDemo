ae4ee94e99380fdca81322b1eea3c2560d8475dc
Paul Batum <paul.batum@gmail.com>
2011-06-04 19:41:20 -0700

    Initial commit of the perfmon demo I live-coded at Portland Code Camp 2011

---
+++ PerfmonDemoPCC/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Microsoft.ServiceModel.WebSockets;
+
+namespace PerfmonDemoPCC
+{
+    public class Global : System.Web.HttpApplication
+    {
+        private WebSocketsHost<MonitoringService> host;
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            host = new WebSocketsHost<MonitoringService>();
+            host.AddWebSocketsEndpoint("ws://localhost:4502/monitoring");
+            host.Open();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PerfmonDemoPCC/MonitoringService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Json;
+using System.Linq;
+using System.Web;
+using Microsoft.ServiceModel.WebSockets;
+using System.Timers;
+
+namespace PerfmonDemoPCC
+{
+    public class MonitoringService : WebSocketsService
+    {
+        private PerformanceCounter counter;
+        private Timer timer;
+
+        public override void OnOpen()
+        {
+            base.OnOpen();
+            counter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+            timer = new Timer(100);
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if(timer != null)
+                    timer.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            decimal value = Convert.ToDecimal(counter.NextValue());
+            DateTime time = DateTime.Now;
+
+            dynamic message = new JsonObject();
+            message.time = time;
+            message.value = value;
+
+            SendMessage(message.ToString());
+        }
+
+        protected override void OnClose(object sender, EventArgs e)
+        {
+            base.OnClose(sender, e);
+            timer.Stop();
+        }
+    }
+}
