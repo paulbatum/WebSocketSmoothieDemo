ee708ddad7f1be8f5b31400948247d8ae7d228b4
Paul Batum <paul.batum@gmail.com>
2011-07-24 18:34:04 -0700

    Moved to console project for server

---
+++ Host/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.ServiceModel.WebSockets;
+using PerfmonDemoPCC;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebSocketsHost<MonitoringService>();
+            host.AddWebSocketsEndpoint("ws://localhost:4502/monitoring");
+            host.Open();
+
+            Console.ReadLine();
+        }
+    }
+}
